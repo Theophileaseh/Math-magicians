01c32b3e1bbd765b723be8e8df9886da2be63ff4
Theophileaseh <theophilenwachan@gmail.com>
2022-04-27 15:54:16 +0100

    Build complete website with pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import Calculator from './components/Calculator';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Calculators from './pages/Calculator/Calculator';
+import Home from './pages/Home/Home';
+import Quote from './pages/Quote/Quote';
+import Header from './components/Navigation';
 
-const App = () => <Calculator />;
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" className="links border" exact element={<Home />} />
+      <Route path="/calculator" className="links border" exact element={<Calculators />} />
+      <Route path="/quote" className="links" exact element={<Quote />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className="maincontainer">
+    <h2 className="intro">Mathematics is not about number, equations, computations, or algorithms: it is about understanding. William Paul Thurston</h2>
+  </div>
+);
+
+export default Home;
